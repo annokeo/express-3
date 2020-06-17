bd87f5c9e0a10be76c28eb3b896ae6922eebdcd2
Anthony T Nokeo <55857508+annokeo@users.noreply.github.com>
2020-06-17 10:01:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,17 +11,35 @@ var db_admin = credentials.getCredentials();
 //monk will be our db connection tool
 var monk = require('monk');
 //connection to Atlas
-var uri = "mongodb+srv://" + db_admin.username + ":" + db_admin.password + "@cluster0-i3nnd.gcp.mongodb.net/test?retryWrites=true&w=majority";
+var uri = "mongodb+srv://" + db_admin.username + ":" + db_admin.password + "@cluster0-i3nnd.gcp.mongodb.net/test_db?retryWrites=true&w=majority";
 var db = monk(uri);
 
 db.then(()=>{
   console.log('Connected to server');
 });
 
+const collection = db.get('test_collection');
+
+collection.find({}, function(err, docs){
+    if(err){
+      console.log(err);
+    }else{
+	console.log(docs);
+    }
+});
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.get('/collections/:name',function(req,res){
+  var collection = db.get(req.params.name);
+  collection.find({},{limit:20},function(e,docs){
+    res.json(docs);
+  })
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
